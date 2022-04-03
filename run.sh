ffe20fb32b340bd6d94c9f3ad565bdf168ae3bf1
DanArmor <danilamorozov07@mail.ru>
2022-04-03 12:47:16 +0300

    Теперь копируются тестовые проги для билда

--- run.sh
+++ run.sh
@@ -3,4 +3,16 @@ cmake .. &&
 make Main &&
 #./TEST &&
 #valgrind ./testList
+cd ..
+
+for file in ./prog*
+do
+    cp $file ./build/
+    if [[ $? -ne 0 ]] ; then
+        printf "Error during coping pascal programs!\n"
+        exit 1
+    fi
+done
+
+cd build
 ./Main
