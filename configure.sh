87a35a5c6351eac69418d2a935a9fab96c439ca1
DanArmor <39347109+DanArmor@users.noreply.github.com>
2022-04-21 21:09:54 +0300

    Create c-cpp.yml
    scripts
    
    scripts
    
    Update c-cpp.yml
    
    scripts
    
    Стандарт 17
    
    scripts
    
    scripts
    
    scripts
    
    scripts
    
    Update c-cpp.yml
    
    Update c-cpp.yml
    
    scripts
    
    scripts

---
+++ configure.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt install gcc-11 g++-11
+sudo apt install cmake
+sudo apt install libfmt-dev
+sudo apt install googletest
+sudo apt install libgtest-dev
+sudo apt install valgrind
---
+++ valgrindTest.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+./allBuild.sh
+
+if [[ $? -ne 0 ]] ; then
+    printf "Error during execution of allBuild.sh!\n"
+    exit 1
+fi
+
+for file in ./tests/lexer/text*
+do
+    cp "./$file" "./build/tests/"
+    if [[ $? -ne 0 ]] ; then
+        printf "Error during coping valgrind test data for lexer!\n"
+        exit 1
+    fi
+done
+
+for file in ./tests/syntax/text*
+do
+    cp "./$file" "./build/tests/"
+    if [[ $? -ne 0 ]] ; then
+        printf "Error during coping valgrind test data for syntax!\n"
+        exit 1
+    fi
+done
+
+for file in ./build/tests/test*
+do
+    valgrind --leak-check=full --error-exitcode=1 "./$file"
+    if [[ $? -ne 0 ]] ; then
+        printf "Error during execution of valgrind tests!\n"
+        exit 1
+    fi
+done
