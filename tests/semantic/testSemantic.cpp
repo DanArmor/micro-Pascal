dc39a46e37dd33f100db0d3a71539cba5f59c2c9
DanArmor <danilamorozov07@mail.ru>
2022-05-03 23:18:14 +0300

    тесты

--- tests/semantic/testSemantic.cpp
+++ tests/semantic/testSemantic.cpp
@@ -88,6 +88,18 @@ TEST(SEMANTIC, PASCAL_6_THROW){
     EXPECT_THROW(root->accept(semantic), SemanticException);
 }
 
+/// @brief Тест семантического анализа Паскаля
+TEST(SEMANTIC, PASCAL_7_THROW){
+    Lexer lexer;
+    lexer.setTemplates(PascalRules::getPascalTemplates());
+    List<Token> tokens = lexer.analyzeFile("./build/tests/textM7.txt");
+
+    SyntaxAnalyzer syntax(tokens);
+    std::unique_ptr<AST> root(syntax.parseTokens());
+
+    SemanticVisitor semantic;
+    EXPECT_THROW(root->accept(semantic), SemanticException);
+}
 
 int main(void){
     testing::InitGoogleTest();
