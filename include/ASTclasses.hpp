05c235b7d8eccea8b36aba372fbea9fe960f7491
DanArmor <danilamorozov07@mail.ru>
2022-05-04 23:20:26 +0300

    Перестановки кода в Visitor

--- include/ASTclasses.hpp
+++ include/ASTclasses.hpp
@@ -2,28 +2,28 @@
 #define __INC_ASTCLASSES_H
 
 // Предварительные объявления
-class BinOpAST;
-class NumberAST;
-class UnOpAST;
-class CompoundAST;
-class AssignAST;
-class VarAST;
-class NoOpAST;
 class ProgramAST;
+class FunctionAST;
 class BlockAST;
 class VarDeclAST;
 class TypeSpecAST;
+class ArrSpecAST;
 class ConstAST;
+class CompoundAST;
+class NumberAST;
 class StringAST;
+class BinOpAST;
+class UnOpAST;
+class NoOpAST;
+class AssignAST;
+class VarAST;
+class SelectAST;
 class CallAST;
+class ReturnAST;
 class IfAST;
 class WhileAST;
 class ForAST;
 class IterationAST;
-class FunctionAST;
-class ReturnAST;
-class ArrSpecAST;
-class SelectAST;
 
 /**
  * @brief Интерфейс "посетителя"
@@ -32,28 +32,28 @@ class SelectAST;
  */
 class IVisitor{
     public:
-    virtual void visit(BinOpAST &node) = 0;
-    virtual void visit(NumberAST &node) = 0;
-    virtual void visit(UnOpAST &node) = 0;
-    virtual void visit(CompoundAST &node) = 0;
-    virtual void visit(AssignAST &node) = 0;
-    virtual void visit(VarAST &node) = 0;
-    virtual void visit(NoOpAST &node) = 0;
     virtual void visit(ProgramAST &node) = 0;
+    virtual void visit(FunctionAST &node) = 0;
     virtual void visit(BlockAST &node) = 0;
     virtual void visit(VarDeclAST &node) = 0;
     virtual void visit(TypeSpecAST &node) = 0;
+    virtual void visit(ArrSpecAST &node) = 0;
     virtual void visit(ConstAST &node) = 0;
+    virtual void visit(CompoundAST &node) = 0;
+    virtual void visit(NumberAST &node) = 0;
     virtual void visit(StringAST &node) = 0;
+    virtual void visit(BinOpAST &node) = 0;
+    virtual void visit(UnOpAST &node) = 0;
+    virtual void visit(NoOpAST &node) = 0;
+    virtual void visit(AssignAST &node) = 0;
+    virtual void visit(VarAST &node) = 0;
+    virtual void visit(SelectAST &node) = 0;
     virtual void visit(CallAST &node) = 0;
+    virtual void visit(ReturnAST &node) = 0;
     virtual void visit(IfAST &node) = 0;
     virtual void visit(WhileAST &node) = 0;
     virtual void visit(IterationAST &node) = 0;
     virtual void visit(ForAST &node) = 0;
-    virtual void visit(FunctionAST &node) = 0;
-    virtual void visit(ReturnAST &node) = 0;
-    virtual void visit(ArrSpecAST &node) = 0;
-    virtual void visit(SelectAST &node) = 0;
 };
 
 /// @brief Базовый класс узлов абстрактного синтаксического дерева
